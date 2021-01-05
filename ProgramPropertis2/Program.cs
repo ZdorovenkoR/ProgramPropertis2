8510425b4e48d2ddb726ca0d145950bd9289c181
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2021-01-05 22:54:25 +0600

    Запрос данных из настроек приложения
    Урок 8

--- ProgramPropertis2/Program.cs
+++ ProgramPropertis2/Program.cs
@@ -10,6 +10,28 @@ namespace ProgramPropertis2
     {
         static void Main(string[] args)
         {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                Console.WriteLine("Введите имя пользователя");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Age))
+            {
+                Console.WriteLine("Введите возраст пользователя");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Activity))
+            {
+                Console.WriteLine("Введите род деятельности пользователя");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string userName = Properties.Settings.Default.UserName;
+            string ageUser = Properties.Settings.Default.Age;
+            string activityUser = Properties.Settings.Default.Activity;
+            Console.WriteLine("Имя пользователя:{0}; Возраст:{1}; Род деятельности:{2}", userName, ageUser, activityUser);
         }
     }
 }
